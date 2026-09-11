4a8850d4b52ee958d064dfe79bebb4aec854a81e
lucasdocente <lucas.adabo@docente.senai.br>
2026-09-11 11:21:57 -0300

    chore: adiciona dist/sw.js

---
+++ dist/sw.js
@@ -0,0 +1,5 @@
+const CACHE='etiqueta-mecanica-v1';
+const ASSETS=['/','/index.html','/styles.css','/pdf.js','/app.js','/favicon.svg','/manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));});
